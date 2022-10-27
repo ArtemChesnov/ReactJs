71591044918da8811d6318e87eb90e3fd88e5ef6
Artem Chesnov <jaksan37@gmail.com>
2022-10-27 18:43:34 +0300

    create users

--- users/src/App.js
+++ users/src/App.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import { Success } from './components/Success'
+import { Users } from './components/Users'
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+
+  useEffect(() => {
+    fetch('https://reqres.in/api/users')
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data)
+      })
+      .catch((err) => {
+        console.warn(err)
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        onChangeSearchValue={onChangeSearchValue}
+        searchValue={searchValue}
+        items={users}
+        isLoading={isLoading}
+      />
       {/* <Success /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
